757bef4ff36e74522e83b16c210f6d054e30077e
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-21 16:51:52 +0000

    ensures the random choice uses return.

--- manual_rps.py
+++ manual_rps.py
@@ -3,6 +3,7 @@ import random
 def get_computer_choice():
     rps = [ "Rock", "Paper", "Scissors"]
     choice = random.choice(rps)
+    return choice
 
 def get_user_choice():
     req = input("Please make a choice.")
