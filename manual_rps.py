2d0902ae375cfcd3a8c354cb79a9090c6f384a21
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-21 20:35:55 +0000

    Fixes text

--- manual_rps.py
+++ manual_rps.py
@@ -16,14 +16,14 @@ def get_winner(computer_choice,user_choice):
         if computer_choice == "Scissors":
             print("You win!")
         else:
-            print("You lost")
+            print("You lost.")
     elif user_choice == "Paper":
         if computer_choice == "Rock":
             print("You win!")
         else:
-            print("You lost")
+            print("You lost.")
     elif user_choice == "Scissors":
         if computer_choice == "Paper":
             print("You win!")
         else:
-            print("You lost")
+            print("You lost.")
