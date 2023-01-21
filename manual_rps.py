a3b9b1c51628c42f65561dac89ef1dc4c0f767e3
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-21 19:54:54 +0000

    Fixes the message when they tie.

--- manual_rps.py
+++ manual_rps.py
@@ -8,9 +8,10 @@ def get_computer_choice():
 def get_user_choice():
     req = input("Please enter Rock, Paper or Scissors")
     return req
+
 def get_winner(computer_choice,user_choice):
     if computer_choice == user_choice:
-        print(f"Both players selected {user_choice}. It's a tie!")
+        print("It's a tie!")
     elif user_choice == "Rock":
         if computer_choice == "Scissors":
             print("You win!")
