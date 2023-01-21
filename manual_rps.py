58724d61556cc48114c104be9f58eb0fd45579f8
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-21 20:46:28 +0000

    refactor get_winner

--- manual_rps.py
+++ manual_rps.py
@@ -9,21 +9,14 @@ def get_user_choice():
     req = input("Please enter Rock, Paper or Scissors")
     return req
 
-def get_winner(computer_choice,user_choice):
+def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
-        print("It's a tie!")
-    elif user_choice == "Rock":
-        if computer_choice == "Scissors":
-            print("You win!")
-        else:
-            print("You lost.")
-    elif user_choice == "Paper":
-        if computer_choice == "Rock":
-            print("You win!")
-        else:
-            print("You lost.")
-    elif user_choice == "Scissors":
-        if computer_choice == "Paper":
-            print("You win!")
-        else:
-            print("You lost.")
+        result = "Draw"
+    # Combinations for user to win
+    elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors"):
+        result = "User Wins!"
+    # All other Combinations means Computer wins
+    else:
+        result = "Computer Wins!"
+    print(result)
+    return result
