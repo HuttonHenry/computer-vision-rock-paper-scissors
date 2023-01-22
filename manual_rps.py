5f85b845fb747dcb5fa24b7f2ab88a5075f1c18a
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-22 08:50:50 +0000

    Missing a winning condition

--- manual_rps.py
+++ manual_rps.py
@@ -12,8 +12,8 @@ def get_user_choice():
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
         winner = "It's a tie!"
-    # Combinations for user to win
-    elif (computer_choice == "Rock" and user_choice == "Paper") or (computer_choice == "Paper" and user_choice == "Scissors"):
+    # User wins under these conditions.
+    elif (computer_choice == "Paper" and user_choice == "Scissors") or (computer_choice == "Rock" and user_choice == "Paper") or (computer_choice == "Scissors" and user_choice == "Rock") :
         winner = "You won!"
     # Otherswise the computer wins
     else:
